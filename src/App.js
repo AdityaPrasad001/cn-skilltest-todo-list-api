fa12e00c65c58ae1d8a0abb25ee2da9442d3c5d9
Aditya <prasad.aditya000@gmail.com>
2023-03-09 01:11:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styles from "./App.module.css";
+import TodoContainer from "./components/TodoContainer";
 
 function App() {
+  const [task, setTask] = useState([]);
+
+  const addTodo = () => {
+    let newTask = document.getElementById("new-task");
+    let newTaskObj = {
+      completed: false,
+      title: newTask.value,
+      id: Date.now(),
+    };
+    task.push(newTaskObj);
+    setTask(task);
+    newTask.value = "";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <div className={styles["todo-bar-container"]}>
+        <div className={styles["todo-bar"]}>
+          <div
+            style={{ borderRadius: "10px 0 0 10px" }}
+            className={styles["icon-container"]}
+          >
+            <i className="fa-solid fa-list"></i>
+          </div>
+          <input
+            id="new-task"
+            type={"text"}
+            placeholder="Enter the task"
+            autoComplete="off"
+          />
+          <div
+            style={{ borderRadius: "0 10px 10px 0" }}
+            className={styles["icon-container"]}
+            id="add-btn"
+            onClick={addTodo}
+          >
+            <i className="fa-solid fa-plus"></i>
+          </div>
+        </div>
+      </div>
+      {/* Todo Container Component */}
+      <TodoContainer addedTask={task} />
     </div>
   );
 }
